d2087ff01b05661ba56a94b53f07efbeebaaafb6
omeraladen <killerpop1738@gmail.com>
2022-08-16 21:49:43 +0200

    woking on portfolio secion with projects

--- src/component/Work/mywork.js
+++ src/component/Work/mywork.js
@@ -1,22 +1,41 @@
-
-import { faReact } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+// import { faReact } from '@fortawesome/free-brands-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import AnimatedLetters from '../AnimatedLetter/animate';
 import './mywork.scss';
+import portfolioData from '../../data/portfolio.json'
+
 
 const Work = () => {
-    
+  
+
+    const renderPortfolio = (portfolio) => {
+        return (
+            <div className="images-container">
+                {
+                    portfolio.map((port, idx) => {
+                        return (
+                            <div className="image-box" key={idx}>
+                                <img src={port.cover} alt="portfolio" />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+
     return (
         <div className="container about-page">
             <div className="text-zone">
                 <h1>
-                    <AnimatedLetters 
+                    <AnimatedLetters className='animate-text'
                     strArray={['M','y',' ','w','o','r','k']}
                                     
                     idx={15}
                     />
                 </h1>
-              
+              <div>{renderPortfolio(portfolioData.portfolio)}</div>
             </div>
 
             <div className="stage-cube-cont">
