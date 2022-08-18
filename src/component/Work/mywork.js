d834a99d2de1d6749fff4fe49b494ad59fcf2ec5
omeraladen <killerpop1738@gmail.com>
2022-08-18 21:44:52 +0200

    done with portfolio section

--- src/component/Work/mywork.js
+++ src/component/Work/mywork.js
@@ -16,7 +16,18 @@ const Work = () => {
                     portfolio.map((port, idx) => {
                         return (
                             <div className="image-box" key={idx}>
-                                <img src={port.cover} alt="portfolio" />
+                                <img src={port.cover} 
+                                     className='portfolio-image' 
+                                     alt="portfolio" />
+
+                                <div className="content">
+                                    <p className='title'>{port.title}</p>
+                                    <h4 className='description'>{port.description}</h4>
+                                    <button
+                                        className='btn'
+                                        onClick={() => window.open(port.url)}
+                                    >View</button>
+                                </div>
                             </div>
                         )
                     })
@@ -27,9 +38,10 @@ const Work = () => {
 
     return (
         <div className="container about-page">
-            <div className="text-zone">
+            <div className="page-title">
                 <h1>
                     <AnimatedLetters className='animate-text'
+                    
                     strArray={['M','y',' ','w','o','r','k']}
                                     
                     idx={15}
